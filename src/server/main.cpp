dc756e0513a5fc39bfe51046ab8c619ba0ffb98f
velnias75 <heiko@rangun.de>
2014-12-23 19:02:14 +0100

    configurable AI player image

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -368,20 +368,6 @@ int main(int argc, const char **argv) {
 
 				logger(PACKAGE_NAME << " - Copyright (c) " << dateOut
 					   << ", Heiko Schaefer <heiko@rangun.de>");
-				logger("");
-				logger(PACKAGE_NAME << " is free software: you can redistribute it and/or modify" \
-					   " it under the terms of the GNU Lesser General Public License as "\
-					   "published by the Free Software Foundation, either version 3 of the "\
-					   "License, or (at your option) any later version.");
-				logger("");
-				logger(PACKAGE_NAME << " is distributed in the hope that it will be useful, but " \
-					   "WITHOUT ANY WARRANTY; without even the implied warranty of " \
-					   "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser" \
-					   " General Public License for more details.");
-				logger("");
-				logger("You should have received a copy of the GNU Lesser General Public "\
-					   "License along with " << PACKAGE_NAME << ".  If not, see "\
-					   "<http://www.gnu.org/licenses/>.");
 			}
 
 #endif
